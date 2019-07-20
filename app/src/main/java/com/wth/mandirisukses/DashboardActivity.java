2ac3f3facb1d69becae16801402e8cb07f8f74f4
Putra <putra.nugraha@dana.id>
2019-07-20 20:36:55 +0700

    Adding layout

---
+++ app/src/main/java/com/wth/mandirisukses/DashboardActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Mandiri What The Hack! 2019
+ * Bank Mandiri
+ * Kalibrr
+ * Accenture
+ * Microsoft
+ * RedHat
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+
+package com.wth.mandirisukses;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author Putra Nugraha (putra.graha.7@gmail.com)
+ * @version DashboardActivity.java, 2019-07-20 20:32 by Putra Nugraha
+ */
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
