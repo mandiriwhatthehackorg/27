a9c0d20c9f4aa4e7319a0cf115897cdc31843e87
Putra <putra.nugraha@dana.id>
2019-07-20 23:07:33 +0700

    Finished welcome and dashboard

---
+++ app/src/main/java/com/wth/mandirisukses/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.wth.mandirisukses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
+            SplashActivity.this.startActivity(intent);
+        }, 2000);
+    }
+}
