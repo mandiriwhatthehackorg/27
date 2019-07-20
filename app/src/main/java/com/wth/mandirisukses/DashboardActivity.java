91b1fcb13dbca32e280e8d2ff0e0a093e6659d5e
Putra <putra.nugraha@dana.id>
2019-07-20 21:39:23 +0700

    Working on dashboard

--- app/src/main/java/com/wth/mandirisukses/DashboardActivity.java
+++ app/src/main/java/com/wth/mandirisukses/DashboardActivity.java
@@ -14,6 +14,14 @@ package com.wth.mandirisukses;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wth.mandirisukses.adapter.PromoAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * @author Putra Nugraha (putra.graha.7@gmail.com)
@@ -22,9 +30,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    @BindView(R.id.recycler_dashboard_promo)
+    RecyclerView recyclerViewDashboardPromo;
+
+    private PromoAdapter promoAdapter;
+    private Unbinder unbinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        unbinder = ButterKnife.bind(this);
+        initPromoRecycler();
+    }
+
+    private void initPromoRecycler() {
+        int[] imageArray = {R.drawable.promo, R.drawable.promo, R.drawable.promo};
+        promoAdapter = new PromoAdapter(this);
+        promoAdapter.setPromoCollection(imageArray);
+        promoAdapter.setOnItemClickListener(() -> {
+            // TODO intent here
+        });
+
+        recyclerViewDashboardPromo.setLayoutManager(
+                new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL,
+                false));
+        recyclerViewDashboardPromo.setAdapter(promoAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
---
+++ app/src/main/java/com/wth/mandirisukses/adapter/PromoAdapter.java
@@ -0,0 +1,101 @@
+/*
+ * Mandiri What The Hack! 2019
+ * Bank Mandiri
+ * Kalibrr
+ * Accenture
+ * Microsoft
+ * RedHat
+ * Copyright (c) 2019 All Rights Reserved.
+ */
+
+package com.wth.mandirisukses.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.wth.mandirisukses.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author Putra Nugraha (putra.graha.7@gmail.com)
+ * @version PromoAdapter.java, 2019-07-20 20:49 by Putra Nugraha
+ */
+
+public class PromoAdapter extends RecyclerView.Adapter<PromoAdapter.PromoViewHolder>  {
+
+    public interface OnItemClickListener {
+        void onItemClicked();
+    }
+
+    private int[] promoCollection;
+    private OnItemClickListener onItemClickListener;
+
+    private final LayoutInflater layoutInflater;
+
+    public PromoAdapter(Context context) {
+        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @NonNull
+    @Override
+    public PromoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = this.layoutInflater.inflate(R.layout.row_promo, parent, false);
+        return new PromoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PromoViewHolder holder, int position) {
+        final int promo = this.promoCollection[position];
+        holder.imageViewPoster.setImageResource(promo);
+        holder.imageViewPoster.setOnClickListener(view -> {
+            if (PromoAdapter.this.onItemClickListener != null) {
+                PromoAdapter.this.onItemClickListener.onItemClicked();
+            }
+        });
+    }
+
+    @Override public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return (this.promoCollection != null) ? this.promoCollection.length : 0;
+    }
+
+    public void setPromoCollection(int[] promoCollection) {
+        this.validatePromoCollection(promoCollection);
+        this.promoCollection = promoCollection;
+        this.notifyDataSetChanged();
+    }
+
+    public void setOnItemClickListener (OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    private void validatePromoCollection(int[] promoCollection) {
+        if (promoCollection == null) {
+            throw new IllegalArgumentException("The list cannot be null");
+        }
+    }
+
+    static class PromoViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.iv_row_promo_image)
+        ImageView imageViewPoster;
+
+        PromoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
