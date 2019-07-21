a0523f05065782a572e89b80c963efe4ad765f09
Putra <putra.nugraha@dana.id>
2019-07-21 07:21:02 +0700

    Added register page layout

--- app/src/main/java/com/wth/mandirisukses/activity/RegisterActivity.java
+++ app/src/main/java/com/wth/mandirisukses/activity/RegisterActivity.java
@@ -2,15 +2,40 @@ package com.wth.mandirisukses.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.wth.mandirisukses.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_landing_login2)
+    Button btnRegister;
+
+    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        unbinder = ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_landing_login2)
+    void navigateTo() {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
